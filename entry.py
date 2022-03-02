351b7bb093c1ae89518c86daccbc52f6eb17660e
EMiulli <eric.miulli@outlook.com>
2022-03-02 14:10:27 -0600

    add if name == main

--- entry.py
+++ entry.py
@@ -4,8 +4,7 @@ root = Tk()
 root.title("Entry Program")
 root.geometry("400x400")
 
-
-def main():
+def myClick():
     hello = "Hello " + e.get()
     myLabel = Label(root, text=hello)
     e.delete(0, 'end')
@@ -14,9 +13,10 @@ def main():
 e = Entry(root, width=50, font=('Helvetica', 30))
 e.pack(padx=10, pady=10)
 
-myButton = Button(root, text="Enter Your Name", command=main)
+myButton = Button(root, text="Enter Your Name", command=myClick)
 myButton.pack(pady=10)
 
 root.mainloop()
 
-
+if __name__ == "__main__":
+    myClick()
