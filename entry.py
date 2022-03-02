04638376ea7496db68c618f57dc1686215cb41f5
EMiulli <eric.miulli@outlook.com>
2022-03-02 14:18:18 -0600

    Changed function name

--- entry.py
+++ entry.py
@@ -4,19 +4,21 @@ root = Tk()
 root.title("Entry Program")
 root.geometry("400x400")
 
-def myClick():
+
+def message():
     hello = "Hello " + e.get()
-    myLabel = Label(root, text=hello)
+    my_label = Label(root, text=hello)
     e.delete(0, 'end')
-    myLabel.pack(pady=10)
+    my_label.pack(pady=10)
+
 
 e = Entry(root, width=50, font=('Helvetica', 30))
 e.pack(padx=10, pady=10)
 
-myButton = Button(root, text="Enter Your Name", command=myClick)
+myButton = Button(root, text="Enter Your Name", command=message)
 myButton.pack(pady=10)
 
 root.mainloop()
 
 if __name__ == "__main__":
-    myClick()
+    message()
